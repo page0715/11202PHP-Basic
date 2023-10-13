f5055160bc96990dc2dcdff2a63a534f07bc4b98
page0715 <page0715@gmail.com>
2023-10-13 14:43:12 +0800

    菱形

--- pra04.php
+++ pra04.php
@@ -59,3 +59,21 @@ for($i=4;$i>=0;$i--){
 echo "<hr>";
 ?>
 <h2>菱形</h2>
+<?php
+for($i=0;$i<9;$i++){
+
+    if($i<=4){
+        $tmp=$i;
+    }else{
+        $tmp--;
+    }
+
+    for($j=0;$j<(4-$tmp);$j++){
+        echo "&nbsp;";
+    }
+    for($k=0;$k<($tmp*2+1);$k++){
+        echo "*";
+    }
+    echo "<br>";
+}
+?>
