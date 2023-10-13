9584bb8c23cdf1ad18465e737e67607dc6f98e18
page0715 <page0715@gmail.com>
2023-10-13 13:22:18 +0800

    直角三角型

---
+++ pra03.php
@@ -0,0 +1,28 @@
+<h1>尋找字串</h1>
+<?php
+$source="印出尋找到的單字或字母所在句子中的位置";
+$target="字母";
+
+$flag="沒找到";
+$start=0;
+// $pos=0;
+while($flag == "沒找到" ){
+    $tmp = mb_substr($source,$start,mb_strlen($target));
+    if($tmp == $target){
+        $flag="找到";
+        break;
+    }
+    $start++;
+    // $pos++;
+
+}
+
+echo "找到了;位置在".$start+1;
+echo "<br>";
+echo "原始字串-$source";
+echo "<br>";
+echo "尋找目標-$target";
+
+
+
+?>
---
+++ pra04.php
@@ -0,0 +1,22 @@
+<?php
+
+echo "*<br>";
+echo "**<br>";
+echo "***<br>";
+echo "****<br>";
+echo "****<br>";
+echo "*****<br>";
+
+echo "<hr>";
+
+for($i=0;$i<5;$i++){
+    for($j=0;$j<=$i;$j++){
+        echo "*";
+    }
+    echo "<br>";
+}
+
+
+
+
+?>
