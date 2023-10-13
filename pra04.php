11061b10a272453a35d04399aa96ce31bb9edd64
page0715 <page0715@gmail.com>
2023-10-13 14:26:43 +0800

    正三角形和倒三角形練習

--- pra04.php
+++ pra04.php
@@ -4,7 +4,6 @@ echo "*<br>";
 echo "**<br>";
 echo "***<br>";
 echo "****<br>";
-echo "****<br>";
 echo "*****<br>";
 
 echo "<hr>";
@@ -15,8 +14,48 @@ for($i=0;$i<5;$i++){
     }
     echo "<br>";
 }
-
-
-
+echo "<hr>";
+echo "<h2>倒直角三角形</h2>";
+for($i=0;$i<=5;$i++){
+    for($j=5;$j>$i;$j--){
+        echo "*";
+    }
+    echo "<br>";
+}
+echo "<hr>";
 
 ?>
+
+
+<style>
+    * {
+        font-family: 'Courier New', Courier, monospace;
+    }
+</style>
+<h2>正三角形</h2>
+<?php
+for($i=0;$i<5;$i++){
+    for($j=0;$j<(4-$i);$j++){
+        echo "&nbsp;";
+    }
+    for($k=0;$k<($i*2+1);$k++){
+        echo "*";
+    }
+    echo "<br>";
+}
+echo "<hr>";
+?>
+<h2>倒正三角形</h2>
+<?php
+for($i=4;$i>=0;$i--){
+    for($j=0;$j<(4-$i);$j++){
+        echo "&nbsp;";
+    }
+    for($k=0;$k<($i*2+1);$k++){
+        echo "*";
+    }
+    echo "<br>";
+}
+echo "<hr>";
+?>
+<h2>菱形</h2>
