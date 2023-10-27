fe4b5228092e9b7f8d4bd5694d17c14a4ddea1fc
page0715 <page0715@gmail.com>
2023-10-27 14:27:43 +0800

    Login 登入

--- PHP/bmi.php
+++ PHP/bmi.php
@@ -7,6 +7,14 @@
 </head>
 <body>
     <h1>計算BMI</h1>
+<?php
+if(!isset($_GET['bmi'])){
+
+    if(isset($_GET['m'])){
+        echo "<span style='color:red'>".$_GET['m']."</span>";
+    }
+?>
+
     <form action="calc.php" method="get">
         <div>
             <label for="height">身高(公尺):</label>
@@ -15,26 +23,28 @@
         <!--
         體重(公斤)/身高(公尺)的平方
         -->
-
-
         <div>
             <label for="pw">體重(公斤):</label>
             <input type="text" name="weight" id="weight">
         </div>
-    
-
-
-
         <div>
             <input type="submit" value="計算BMI">
             <input type="reset" value="重置">
         </div>
     </form>
+<?php
+}else{
+    echo "體重:".$_GET['w']."公斤";
+    echo "<BR>";
+    echo "身高:".$_GET['h']."公尺";
+    echo "<BR>";
+    echo "BMI:".$_GET['bmi'];  
+    echo "<BR>";
 
+}
+?>
 </body>
 </html>
-
- 
 <?php
 
 ?>
--- PHP/calc.php
+++ PHP/calc.php
@@ -1,13 +1,23 @@
 <?php
-$weight=$_GET['weight'];
-$height=$_GET['height'];
 
-echo "體重:".$weight;
-echo "身高:".$height;
-echo "體重:".$_GET['weight'];
-echo "身高:".$_GET['height'];
+if(!empty($_GET)){ 
 
+    $weight=(!empty($_GET['weight']))?$_GET['weight']:'沒有體重資料';
+    $height=(!empty($_GET['height']))?$_GET['height']:'沒有身高資料';
 
+    $bmi=round($weight/($height*$height),2);
 
-?>
+    
+    
+    // echo "體重:".$weight;
+    // echo "身高:".$height;
+    // echo "BMI:".$bmi;
+    // echo "體重:".$_GET['weight'];
+    // echo "身高:".$_GET['height'];
 
+    header("location:bmi.php?w=$weight&h=$height&bmi=$bmi");
+
+}else{
+    header("location:bmi.php?m=請輸入必要的資訊");
+    // echo "請輸入必要的資訊.";
+}
